4de315c17859e1c34c3c7e14604175c235d6ee66
Ichmed <mijomeyer@t-online.de>
2017-01-09 06:17:20 +0100

    Created IRendered
    Implemented IRendered in Animation and created code for Animation

---
+++ src/com/ichmed/scr3w/grafics/IRendered.java
@@ -0,0 +1,7 @@
+package com.ichmed.scr3w.grafics;
+
+public interface IRendered
+{
+	public void render(float x, float y);
+	public boolean shouldCleanUp();
+}
--- src/com/ichmed/scr3w/grafics/animation/Animation.java
+++ src/com/ichmed/scr3w/grafics/animation/Animation.java
@@ -1,5 +1,43 @@
 package com.ichmed.scr3w.grafics.animation;
 
-public class Animation {
+import com.ichmed.scr3w.grafics.IRendered;
+
+public class Animation implements IRendered
+{
+	private IRendered[] frames;
+	private int frameCounter;
+	private int maxFrames;
+	private boolean shouldLoop;
+	
+	public Animation(String frames[])
+	{
+		this(frames, false);
+	}
+
+	public Animation(String frames[], boolean shouldLoop)
+	{
+		this(frames, shouldLoop, frames.length);
+	}
+	
+	public Animation(String[] frames, boolean shouldLoop, int maxFrames)
+	{
+		this.frames = new IRendered[frames.length];
+		this.maxFrames = maxFrames;
+		this.shouldLoop = shouldLoop;
+	}
+
+	@Override
+	public void render(float x, float y)
+	{
+		frames[frameCounter].render(x, y);
+		if(frameCounter < maxFrames) frameCounter++;
+		else if(shouldLoop) frameCounter = 0;
+	}
+
+	@Override
+	public boolean shouldCleanUp()
+	{
+		return frameCounter > this.maxFrames; 
+	}
 
 }
